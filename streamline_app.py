ba0364a07bda20ff0b3a5ed00d1e746caab269e0
snowflakeusr <133612444+snowflakeusr@users.noreply.github.com>
2023-05-14 20:52:52 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title('My Mom New Health Diner')
 
 streamlit.header 'Breakfast Favorites')
-streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit. text ('O Kale, Spinach & Rocket Smoothie')
-streamlit.text ('<) Hard-Boiled Free-Range Egg')
-streamlit.text('DO Avocado Toast')
+streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
+streamlit. text ('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑 Avocado Toast')
