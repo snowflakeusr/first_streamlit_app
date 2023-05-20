b0b4bc60fd37dc1602e3bac6ad5733c495ca9d08
snowflakeusr <133612444+snowflakeusr@users.noreply.github.com>
2023-05-20 11:30:23 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -29,7 +29,7 @@ import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json ())
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json) # just writes the data to the screen
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json ())
